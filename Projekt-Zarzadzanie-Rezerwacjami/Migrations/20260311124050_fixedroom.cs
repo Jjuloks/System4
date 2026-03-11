afc1df05d6b03716c87620e8a6789909d9e1cd29
INFINITY\j.oksimowicz <j.oksimowicz@infinity-group.pl>
2026-03-11 13:59:04 +0100

    added room displaying in rerservations,

---
+++ Projekt-Zarzadzanie-Rezerwacjami/Migrations/20260311124050_fixedroom.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projekt_Zarzadzanie_Rezerwacjami.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixedroom : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rezerwacja_Room_RoomId",
+                table: "Rezerwacja");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoomId",
+                table: "Rezerwacja",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rezerwacja_Room_RoomId",
+                table: "Rezerwacja",
+                column: "RoomId",
+                principalTable: "Room",
+                principalColumn: "SalaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rezerwacja_Room_RoomId",
+                table: "Rezerwacja");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoomId",
+                table: "Rezerwacja",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rezerwacja_Room_RoomId",
+                table: "Rezerwacja",
+                column: "RoomId",
+                principalTable: "Room",
+                principalColumn: "SalaId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
