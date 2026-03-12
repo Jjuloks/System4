05de242ca05f45911ea3776126211a4cd50bb08b
INFINITY\j.oksimowicz <j.oksimowicz@infinity-group.pl>
2026-03-12 12:06:03 +0100

    Login controller by tymenix

--- Projekt-Zarzadzanie-Rezerwacjami/Controllers/LoginController.cs
+++ Projekt-Zarzadzanie-Rezerwacjami/Controllers/LoginController.cs
@@ -1,41 +1,49 @@
-﻿
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Mvc;
-using Projekt_Zarzadzanie_Rezerwacjami.Models;
-
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Projekt_Zarzadzanie_Rezerwacjami.Data;
+ 
 namespace Projekt_Zarzadzanie_Rezerwacjami.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly Projekt_Zarzadzanie_RezerwacjamiContext _context;
+
+        public LoginController(Projekt_Zarzadzanie_RezerwacjamiContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-       [HttpPost]
-       public IActionResult Logowanie(string login, string password)
+        [HttpPost]
+        public async Task<IActionResult> Logowanie(string login, string password)
         {
-            if (login == "admin" && password == "admin")
+            var user = await _context.Uzytkownik
+                .FirstOrDefaultAsync(x => x.Login == login && x.Password == password);
+
+            if (user != null)
             {
-                return RedirectToAction("","Rezerwacje");
+                HttpContext.Session.SetString("role", user.Role);
+                HttpContext.Session.SetString("login", user.Login);
+
+                if (user.Role == "admin")
+                    return RedirectToAction("Index", "Rezerwacje");
+
+                if (user.Role == "user")
+                    return RedirectToAction("Index", "User");
             }
-            if (login == "user" && password == "user")
-            {
-                //Tu do Usera
-                return RedirectToAction("", "User");
-            }
-            ViewBag.Error = "Niepoprawny dane logowania";
+
+            ViewBag.Error = "Niepoprawne dane logowania";
             return View("Index");
         }
-        public IActionResult Admin()
-        {
-            return View();
-        }
-        public IActionResult User()
-        {
-            return View();
-        }
 
-      
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
     }
 }
